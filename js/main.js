fda129050a451b0f87e0b4acdc251ecdba5f53d1
Алексей <dudarevav86@gmail.com>
2019-01-25 17:55:32 +0600

    media

--- js/main.js
+++ js/main.js
@@ -32,23 +32,9 @@ $(document).ready(function() {
     $('.mobile-button').click(function() {
         if ($('.mobile-button').hasClass('is-active')) {
             $('.mobile-button').removeClass('is-active');
-            $('.header_nav').removeClass('active');
-            $('.mobile-bg-overlay').removeClass('open');
         } else {
             $('.mobile-button').addClass('is-active');
-            $('.header_nav').addClass('active');
-            $('.mobile-bg-overlay').addClass('open');
         }
-    });
-    $('.mobile-bg-overlay').click(function() {
-        $('.header_nav').removeClass('active');
-        $('.mobile-button').removeClass('is-active');
-        $(this).removeClass('open');
-    });
-    $('.header_nav ul li a').click(function() {
-        $('.mobile-bg-overlay').removeClass('open');
-        $('.header_nav').removeClass('active');
-        $('.mobile-button').removeClass('is-active');
     });
 
 })
